8f2d2b71dece9a65a7b163e8c83b418b7d7dc41a
Iván Pabón <176315757+IvanPabon29@users.noreply.github.com>
2024-08-19 14:56:15 -0500

    Modificar Rutas Se cambia para que las rutas funcionen en Github Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Inicio from "./pages/Inicio";
 import Servicios from "./pages/Servicios";
